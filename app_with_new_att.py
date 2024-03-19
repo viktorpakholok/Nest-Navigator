82557d6b396dc9d53a39a3fded35342f1dcb3e17
Bazylevych Sofiia <bazylevych.pn@ucu.edu.ua>
2024-03-19 17:34:42 +0200

    improve

--- app_with_new_att.py
+++ app_with_new_att.py
@@ -92,7 +92,7 @@ def index(page):
     filters = session.get('filters', {})
     query = filter_query(filters)
     query = sort_query(query, filters)
-    apartaments = query.paginate(page, pages, error_out=False)
+    apartaments = query.paginate(page=page, per_page=pages, error_out=False)
     # print(apartaments)
     return render_template('index.html', apartaments=apartaments, filters=filters)
 
