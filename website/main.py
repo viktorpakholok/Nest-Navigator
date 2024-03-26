4a7afca004f81557e9c27ff7688736e725b41f1b
Viktor Pakholok <pakholok.pn@ucu.edu.ua>
2024-03-26 22:52:44 +0200

    updated main.py from conect_back_to_front

--- website/main.py
+++ website/main.py
@@ -5,30 +5,21 @@ The main file which runs the web application
 from flask import Flask, request, session, redirect, url_for, render_template, g
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import desc, asc
-
-from apscheduler.schedulers.background import BackgroundScheduler
-import time
-
-from database_creation import parser_olx, parser_dom, DatabaseManipulation
-
-from flask_cors import CORS
-# from dotenv import load_dotenv
 import os
-
-# load_dotenv()
+import time
 
 
 app = Flask(__name__)
-
-CORS(app)
-
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
-# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://Housesdb_owner:MOGU0lh5ByIg@ep-aged-pine-a29r8a5c.eu-central-1.aws.neon.tech/Housesdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://Housesdb_owner:\
+MOGU0lh5ByIg@ep-aged-pine-a29r8a5c.eu-central-1.aws.neon.tech/Housesdb'
 app.config['SECRET_KEY'] = 'BohdanBohdanBohdan'
 
 db = SQLAlchemy(app)
+
+class Districts(db.Model):
+    __tablename__ = "Districts"
+    city = db.Column(db.String, primary_key = True)
+    districts = db.Column(db.String)
 
 class Apartment(db.Model):
     '''
@@ -48,12 +39,20 @@ class Apartment(db.Model):
     city = db.Column(db.String)
     price_per_meter = db.Column(db.Float)
 
+def get_all_districts_and_cities():
+    alp = 'АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ'
+    query = Districts.query
+    output_dict = {}
+    for i in query:
+        output_dict[i.city] = sorted(i.districts.split(','))
+    return dict(sorted(list(output_dict.items()), key=lambda x: alp.index(x[0][0].upper())))
+
 def filter_query(filters):
     g.query = Apartment.query
     if filters.get('city'):
         g.query = g.query.filter(Apartment.city.like('%' + filters['city'] + '%'))
-    if filters.get('district'):
-        g.query = g.query.filter(Apartment.district.like('%' + filters['district'] + '%'))
+    if filters.get('regions'):
+        g.query = g.query.filter(Apartment.district.in_(filters['regions']))
     if filters.get('min_area'):
         g.query = g.query.filter(Apartment.area >= float(filters['min_area']))
     if filters.get('max_area'):
@@ -102,9 +101,9 @@ def search(page):
     pages = 15
     if request.method == 'POST':
         session_filters = session.get('filters', {})
+        # print(request.form)
         session_filters.update({
             'city': request.form.get('city_tag'),
-            'district': request.form.get('district_tag'),
             'min_area': request.form.get('min_area_tag'),
             'max_area': request.form.get('max_area_tag'),
             'min_price': request.form.get('min_price_tag'),
@@ -115,6 +114,7 @@ def search(page):
             # 'sort_min_price':request.form.get('sort_min_price_tag'),
             # 'sort_max_price/area':request.form.get('sort_max_pricearea_tag'),
             # 'sort_min_price/area':request.form.get('sort_min_pricearea_tag')
+            'regions': request.form.getlist('checkbox-region'),
             'dropdown': request.form.get('dropdown')
         })
         session_filters = {k: v for k, v in session_filters.items() if v is not None}
@@ -124,27 +124,13 @@ def search(page):
     filters = session.get('filters', {})
     g.query = filter_query(filters)
     query = sort_query(g.query, filters)
-
+    print(filters)
     apartaments = query.paginate(page=page, per_page=pages, error_out = False)
+    t_s = time.time()
+    city_dis_dict = get_all_districts_and_cities()
+    print(time.time() - t_s)
+    return render_template('search_page.html', apartaments=apartaments, filters=filters, city_dis_dict = city_dis_dict)
 
-    return render_template('search_page.html', apartaments=apartaments, filters=filters)
-
-def job_function():
-    print('started')
-    my_check_set = set()
-    dict1 = parser_dom(5, my_check_set)
-    # print(dict1)
-    # set2 = parser_olx(1, my_check_set)
-    # print(set2)
-    data = DatabaseManipulation(38.81, 42.28)
-    # data.read_set_to_objects_olx(set2)
-    # start = time.time()
-    data.read_set_to_objects_dom(dict1)
-    print('ended')
-
-# scheduler = BackgroundScheduler()
-# scheduler.add_job(job_function, 'interval', minutes = 2)
-# scheduler.start()
 
 if __name__ == "__main__":
     app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
