ffd4201dbf3c2748956da862f8eaacd6552f4485
Viktor Pakholok <pakholok.viktor@ukr.net>
2024-03-30 18:24:30 +0200

    Fixed problem with sliding left

--- website/static/jscripts/index.js
+++ website/static/jscripts/index.js
@@ -19,3 +19,9 @@ function next(t){
     elm.append(item[0])
     console.log('what')
 }
+
+function prev(t){
+    var elm = t.parentElement.children[1].children[0];
+    var item = elm.getElementsByClassName('item');
+    elm.prepend(item[item.length - 1]);
+}
